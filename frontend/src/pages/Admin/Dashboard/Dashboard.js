26be2ae001067710fdc416f5067150fec3aa5d61
QuocAnU <an.lequoc2103@hcmut.edu.vn>
2023-11-11 19:08:09 +0700

    Sidebar ListPage

---
+++ frontend/src/pages/Admin/Dashboard/Dashboard.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Sidebar from '../../../components/Sidebar/Sidebar'
+
+const Dashboard = () => {
+    return (
+        <div>
+            <div>
+                <Sidebar />
+            </div>
+
+
+            <h1 >WELCOME TO Dashboard</h1>
+            <h3>Bank of the free</h3>
+            <p>Lorem ipsum dolor sit amet...</p>
+
+        </div>
+
+
+
+
+    )
+}
+export default Dashboard
