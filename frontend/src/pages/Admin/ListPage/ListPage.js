c5f66503f441676fc3358f6a2987dfe0d9701787
QuocAnU <an.lequoc2103@hcmut.edu.vn>
2023-11-13 00:07:14 +0700

    Update ListPage

--- frontend/src/pages/Admin/ListPage/ListPage.js
+++ frontend/src/pages/Admin/ListPage/ListPage.js
@@ -1,40 +1,6 @@
-// import React from 'react';
-// import Sidebar from '../../../components/Sidebar/Sidebar';
-// import Container from "react-bootstrap/Container"
-// import Row from "react-bootstrap/Row"
-// import Col from "react-bootstrap/Col"
-// import ShowList from './ShowList';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-// const ListPage = () => {
-//     return (
-//         <div>
-//             <Sidebar />
-//             <Container>
-//                 <Row>
-//                     <Col xs={1}>
-//                     </Col>
-//                     <Col xs={11}>
-//                         <ShowList />
-//                     </Col>
-//                 </Row>
-
-//             </Container>
-
-//         </div>
-
-
-
-
-
-//     )
-// }
-
-// export default ListPage;
 
 import React from 'react';
 import Sidebar from '../../../components/Sidebar/Sidebar';
-import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import ShowList from './ShowList';
@@ -43,18 +9,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const ListPage = () => {
     return (
         <div>
-
-            <Sidebar />
-            <Container>
-                <Row>
-                    <Col xs={12} sm={1}>
-                        {/* On small screens, the sidebar will take up the entire width */}
-                    </Col>
-                    <Col xs={12} sm={11}>
-                        <ShowList />
-                    </Col>
-                </Row>
-            </Container>
+            <Row>
+                <Col xs={2} sm={2}>
+                    <Sidebar />
+                </Col>
+                <Col xs={10} sm={9}>
+                    <ShowList />
+                </Col>
+            </Row>
         </div>
     )
 }
