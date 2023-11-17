eb988e3e504b0ec19ef9da1b36e7b0ce9b676d31
dascy <kaitoukid1305@gmail.com>
2023-11-17 22:16:29 +0700

    post

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -3,11 +3,31 @@ import { db } from "../config/dbConfig.js";
 
 export const createBook = async (req, res) => {
   try {
-    const { ISBN, title, description } = req.body;
-    if (!ISBN || !title || !description) {
+    const {
+      ISBN,
+      title,
+      desc,
+      publisher,
+      publishDate,
+      coverType,
+      noPages,
+      language,
+      author,
+    } = req.body;
+    if (!ISBN || !title || !desc) {
       return res.status(400).send({ message: "Pls send all required fields!" });
     }
-    let book = new Book(ISBN, title, description);
+    let book = new Book(
+      ISBN,
+      title,
+      desc,
+      publisher,
+      publishDate,
+      coverType,
+      noPages,
+      language,
+      author
+    );
     book = await book.save();
     console.log("Create new book");
     return res.status(201).send(book);
@@ -20,8 +40,8 @@ export const createBook = async (req, res) => {
 export const getAllBook = async (req, res) => {
   try {
     let sql = `
-    SELECT ISBN, title, \`desc\`, publisher, publishDate, coverType, noPages, \`language\`, \`name\` 
-FROM (((book natural join edition) natural join author_write_book) join author on author_write_book.authorID=author.id);`;
+    SELECT ISBN, title, \`authorName\`, \`desc\`, publisher, publishDate, coverType, noPages, \`language\` 
+FROM (((book natural join edition) natural join author_write_book) join author on author_write_book.authorID=author.authorID);`;
     const data = await db.execute(sql);
     return res.json(data[0]);
   } catch (error) {
