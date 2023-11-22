0670433dc422bf559b88dee9cd443280724571d7
dascy <kaitoukid1305@gmail.com>
2023-11-22 13:32:28 +0700

    db update + rollback

--- backend/routes/bookRouter.js
+++ backend/routes/bookRouter.js
@@ -3,9 +3,11 @@ import {
   createBook,
   getAllBook,
   updateBookByISBN,
+  DeleteBookByISBN,
 } from "../controllers/bookController.js";
 
 const router = Router();
 router.route("/").get(getAllBook).post(createBook).put(updateBookByISBN);
+router.route("/:isbn").delete(DeleteBookByISBN);
 
 export const bookRouter = router;
