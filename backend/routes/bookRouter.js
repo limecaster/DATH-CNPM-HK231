981b30ef3e2277aad8365c67ffb02f1ca22a038c
dascy <kaitoukid1305@gmail.com>
2023-12-31 14:26:51 +0700

    noti + trending

--- backend/routes/bookRouter.js
+++ backend/routes/bookRouter.js
@@ -9,7 +9,8 @@ import {
   getFavoriteBookOfReader,
   getAllGenres,
   getBookByGenre,
-  SearchBook
+  SearchBook,
+  getTrendingBooks,
 } from "../controllers/bookController.js";
 // /books
 const router = Router();
@@ -18,11 +19,12 @@ router
   .get(getAllBook) // thông tin tất cả các sách
   .post(createBook) // thêm sách
   .put(updateBookByISBN); // chỉnh sửa thông tin sách
+router.route("/trending").get(getTrendingBooks); // list sách xu hướng
 router.route("/favorite/:readerId").get(getFavoriteBookOfReader); // list sách ưa thích theo reader
 router.route("/genres").get(getAllGenres); // list thể loại sách
 router.route("/genres/:genre").get(getBookByGenre); // list sách theo thể loại
 
-router.route('/search').get(async (req, res) => {
+router.route("/search").get(async (req, res) => {
   const searchText = req.query.query;
   try {
     const data = await SearchBook(searchText);
@@ -33,13 +35,10 @@ router.route('/search').get(async (req, res) => {
   }
 });
 
-
 router
   .route("/:isbn")
   .get(getOneBook) // thông tin sách theo ISBN
   .delete(DeleteBookByISBN); // xoá sách
 router.route("/:isbn/genres").get(GetBookGenres); // thể loại sách theo ISBN
-
-
 
 export const bookRouter = router;
