5131b77c67c49e0a17671621cee9ce14d3e6e257
cuongnguyen2323 <cuong.nguyenmsmsms23@hcmut.edu.vn>
2024-01-01 19:23:26 +0700

    add to/ remove from fav list

--- frontend/src/pages/User/BookDetailPage/BookDetailPage.js
+++ frontend/src/pages/User/BookDetailPage/BookDetailPage.js
@@ -1,274 +1,3 @@
-// import axios from "axios";
-// import React, { useEffect, useState } from "react";
-// import { useParams } from "react-router-dom";
-
-// import "./BookDetailPage.css";
-// import useNode from "./useNode";
-// import Comment from "./Comment";
-// import ReadMore from "./ReadMore";
-// const comments = {
-//   id: 1,
-//   items: [],
-// };
-// export default function BookDetailPage() {
-//   const { ISBN } = useParams();
-//   const [books, setBooks] = useState([]);
-//   const [genres, setGenres] = useState([]);
-//   const [commentsData, setCommentsData] = useState(comments);
-
-//   const { insertNode, editNode, deleteNode } = useNode();
-
-//   const handleInsertNode = (folderId, item) => {
-//     const finalStructure = insertNode(commentsData, folderId, item);
-//     setCommentsData(finalStructure);
-//   };
-
-//   const handleEditNode = (folderId, value) => {
-//     const finalStructure = editNode(commentsData, folderId, value);
-//     setCommentsData(finalStructure);
-//   };
-
-//   const handleDeleteNode = (folderId) => {
-//     const finalStructure = deleteNode(commentsData, folderId);
-//     const temp = { ...finalStructure };
-//     setCommentsData(temp);
-//   };
-
-//   useEffect(() => {
-//     axios
-//       .get("http://localhost:3001/books")
-//       .then((res) => {
-//         console.log(res.data);
-//         setBooks(res.data);
-//       })
-//       .catch((err) => {
-//         console.error(err);
-//       });
-//   }, []);
-//   useEffect(() => {
-//     axios
-//       .get(`http://localhost:3001/books/${ISBN}/genres`)
-//       .then((res) => {
-//         console.log(res.data);
-//         setGenres(res.data);
-//       })
-//       .catch((err) => {
-//         console.error(err);
-//       });
-//   }, []);
-//   const book = books.find((b) => String(b.ISBN) === ISBN);
-//   if (!book) return null;
-
-//   return (
-//     <div className="display">
-//       <div>
-//         <img
-//           //src={require("../../../assets/image/book.png")}
-//           src={book.coverLink}
-//           alt="img"
-//           className="img"
-//         />
-
-//         <div className="text">
-//           {/* book title */}
-//           <p
-//             style={{
-//               fontSize: "40px",
-//               fontFamily: "Work Sans",
-//               fontWeight: "700",
-//               marginLeft: "10px",
-//             }}
-//           >
-//             {book.title}
-//           </p>
-//           {/* rating */}
-//           <p>
-//             <div
-//               style={{
-//                 color: "rgb(255, 200, 0)",
-//                 fontSize: "24px",
-//                 marginLeft: "10px",
-//                 display: "inline-block",
-//               }}
-//             >
-//               &#9733;
-//             </div>
-//             <div
-//               style={{
-//                 display: "inline-block",
-//                 fontSize: "20px",
-//                 fontFamily: "Work Sans",
-//                 fontWeight: "600",
-//                 marginLeft: "10px",
-//               }}
-//             >
-//               4,8
-//             </div>
-//             <div
-//               style={{
-//                 display: "inline-block",
-//                 fontSize: "20px",
-//                 fontFamily: "Work Sans",
-//                 color: "grey",
-//                 marginLeft: "10px",
-//               }}
-//             >
-//               (1,873)
-//             </div>
-//           </p>
-//           {/* author */}
-//           <p>
-//             <div
-//               style={{
-//                 display: "inline-block",
-//                 fontSize: "25px",
-//                 fontFamily: "Work Sans",
-//                 fontWeight: "600",
-//                 marginLeft: "10px",
-//               }}
-//             >
-//               Tác giả
-//             </div>
-//             <div
-//               style={{
-//                 display: "inline-block",
-//                 fontSize: "20px",
-//                 fontFamily: "Work Sans",
-//                 color: "grey",
-//                 marginLeft: "10px",
-//               }}
-//             >
-//               {book.authorName}
-//             </div>
-//           </p>
-//           {/* genres */}
-//           <p>
-//             <div
-//               style={{
-//                 display: "inline-block",
-//                 fontSize: "25px",
-//                 fontFamily: "Work Sans",
-//                 fontWeight: "600",
-//                 marginLeft: "10px",
-//               }}
-//             >
-//               Thể loại
-//             </div>
-//             <div
-//               style={{
-//                 display: "inline-block",
-//                 fontSize: "20px",
-//                 fontFamily: "Work Sans",
-//                 color: "grey",
-//                 marginLeft: "10px",
-//               }}
-//             >
-//               <div>
-//                 {genres.map((genre, index) => (
-//                   <React.Fragment key={index}>
-//                     {genre}
-//                     {index !== genres.length - 1 && ", "}
-//                   </React.Fragment>
-//                 ))}
-//               </div>
-//             </div>
-//           </p>
-//           {/* publisher */}
-//           <p>
-//             <div
-//               style={{
-//                 display: "inline-block",
-//                 fontSize: "25px",
-//                 fontFamily: "Work Sans",
-//                 fontWeight: "600",
-//                 marginLeft: "10px",
-//               }}
-//             >
-//               Nhà xuất bản
-//             </div>
-//             <div
-//               style={{
-//                 display: "inline-block",
-//                 fontSize: "20px",
-//                 fontFamily: "Work Sans",
-//                 color: "grey",
-//                 marginLeft: "10px",
-//               }}
-//             >
-//               {book.publisher}
-//             </div>
-//           </p>
-//           {/*  button mượn */}
-//           <p>
-//             <button
-//               className="btn btn-success "
-//               style={{
-//                 width: "200px",
-//                 textAlign: "center",
-//                 color: "white",
-//                 fontSize: 20,
-//                 fontFamily: "Work Sans",
-//                 fontWeight: "400",
-//                 wordWrap: "break-word",
-//                 backgroundColor: "#31AAB7",
-//                 //marginTop: "175px",
-//                 marginLeft: "10px",
-//               }}
-//             >
-//               Mượn sách
-//             </button>
-//           </p>
-//         </div>
-//       </div>
-//       {/* mô tả */}
-//       <div className="description">
-//         <p>
-//           <div
-//             style={{
-//               fontSize: "25px",
-//               fontFamily: "Work Sans",
-//               fontWeight: "600",
-//               //marginLeft: "10px",
-//             }}
-//           >
-//             Mô tả
-//           </div>
-
-//           <div
-//             style={{
-//               fontSize: "20px",
-//               fontFamily: "Work Sans",
-//               color: "grey",
-//               //marginLeft: "10px",
-//             }}
-//           >
-//             <ReadMore text={book.desc} />
-//           </div>
-//         </p>
-//       </div>
-
-//       <div
-//         className="feedback"
-//         style={{
-//           fontSize: "25px",
-//           fontFamily: "Work Sans",
-//           fontWeight: "600",
-//           marginTop: "10px",
-//         }}
-//       >
-//         Phản hồi
-//       </div>
-
-//       <Comment
-//         handleInsertNode={handleInsertNode}
-//         handleEditNode={handleEditNode}
-//         handleDeleteNode={handleDeleteNode}
-//         comment={commentsData}
-//       />
-//     </div>
-//   );
-// }
-
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -285,11 +14,43 @@ const comments = {
 };
 export default function BookDetailPage() {
   const { ISBN } = useParams();
+
   const [books, setBooks] = useState([]);
   const [genres, setGenres] = useState([]);
   const [commentsData, setCommentsData] = useState(comments);
   const [value, setValue] = useState(0);
   const { insertNode, editNode, deleteNode } = useNode();
+
+  const jwtToken = localStorage.getItem("token");
+  const [isHearted, setIsHearted] = useState(false);
+  const handleHeartClick = async (event) => {
+    event.preventDefault();
+    setIsHearted(!isHearted);
+    const readerId = localStorage.getItem("userID");
+
+    const url = "http://localhost:3001/favorite";
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${jwtToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ readerId, ISBN }),
+      });
+      if (response.status === 200) {
+        const responseResult = await response.json();
+        console.log(responseResult);
+
+        alert("đã thêm vào danh sách ưa thích");
+      } else {
+        console.error("failed to add to favorite");
+      }
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+  };
 
   const handleInsertNode = (folderId, item) => {
     const finalStructure = insertNode(commentsData, folderId, item);
@@ -354,7 +115,21 @@ export default function BookDetailPage() {
           >
             {book.title}
           </p>
+          <p
+            style={{
+              fontSize: "20px",
+              fontFamily: "Work Sans",
+              fontWeight: "700",
+              marginLeft: "10px",
+              color: isHearted ? "red" : "black",
+              cursor: "pointer",
+            }}
+            onClick={handleHeartClick}
+          >
+            {isHearted ? "❤️" : "🤍"}
+          </p>
           {/* rating */}
+
           <p>
             <div
               style={{
