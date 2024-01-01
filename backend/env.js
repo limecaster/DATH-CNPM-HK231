9f9a78417d33da2f65eb925bdda85c79720a6fd2
cuson311 <cuson108@gmail.com>
2024-01-02 01:21:11 +0700

    fix header admin

--- backend/env.js
+++ backend/env.js
@@ -2,5 +2,5 @@ export const PORT = 3001;
 export const DB_HOST = "localhost";
 export const DB_USER = "root";
 export const DB_NAME = "library";
-export const DB_PASSWORD = "An1ma@k01";
+export const DB_PASSWORD = "son757303";
 
